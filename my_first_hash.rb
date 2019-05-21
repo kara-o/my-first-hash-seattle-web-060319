f428d2ca5c2f01b59b15d025884029bace40e29e
Kara O <karajo.odle@gmail.com>
2019-05-21 22:28:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
 def my_hash
   your_hash = {
     
-    "husband" => "Alex"
-    "dog" => "Rooney"
+    "husband" => "Alex",
+    "dog" => "Rooney",
     "city" => "Seattle"
     
   }
@@ -17,8 +17,8 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {
     
-    "whale bone corsets" => 5
-    "porcelain vases" => 2 
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2, 
     "oil paintings" => 3
     
   }
